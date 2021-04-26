e8e133f9f3e49403696e87c02086d5b89288bebd
Ye-eun-Kim <8383655@naver.com>
2021-04-26 23:58:23 +0900

    ing...

--- 210426.py
+++ 210426.py
@@ -1,16 +1,24 @@
 def solution(bridge_length, weight, truck_weights):
     time = 0
-    if len(truck_weight) = 1 :
+    if len(truck_weights) == 1 :
         return bridge_length + 1
     else :
         on = []
         for i in (len(bridge_length)-1) :
             on[i] = 0
-        on[len(bridge_length)-1] = turck_weights.pop(0)
+        on[len(bridge_length)-1] = truck_weights.pop(0)
+        time += 1
         
         for j in (len(truck_weights)) :
-            if (sum(bridge_length)+truck_weights[i]) <= weight :
-                bridge_length.append.(pop(truck_weights[i]))
+            if (sum(bridge_length)+truck_weights[j]) <= weight :
+                temp = truck_weights.pop(j)
+                bridge_length.append(temp)
+                del(bridge_length[0])
+                time += 1
+                
+            else : 
+                del(bridge_length[0])
+                time += 1
         
             
         
