6549083e584a84360f317d5fc08bf9292fcd867b
Ye-eun-Kim <8383655@naver.com>
2021-04-27 00:38:42 +0900

    correct only for 3 given test sets

--- 210426.py
+++ 210426.py
@@ -1,26 +1,29 @@
 def solution(bridge_length, weight, truck_weights):
     time = 0
+    ## 차가 1개일 때
     if len(truck_weights) == 1 :
         return bridge_length + 1
+    ## 차가 1개 초과일 때
     else :
+        ## on: 다리 위의 차 상태, bridge_length만큼 0으로 초기화
+        
         on = []
-        for i in (len(bridge_length)-1) :
-            on[i] = 0
-        on[len(bridge_length)-1] = truck_weights.pop(0)
+        for i in range(bridge_length-1) :
+            on.append(0)
+        ## 첫 번째 차
+        on.append(truck_weights.pop(0))
         time += 1
         
-        for j in (len(truck_weights)) :
-            if (sum(bridge_length)+truck_weights[j]) <= weight :
-                temp = truck_weights.pop(j)
-                bridge_length.append(temp)
-                del(bridge_length[0])
+        ## 두 번째 차부터, truck_weights 전부 넣기.
+        while truck_weights :
+            if (sum(on)+truck_weights[0]) <= weight :
+                temp = truck_weights.pop(0)
+                on.append(temp)
+                del(on[0])
                 time += 1
                 
             else : 
-                del(bridge_length[0])
+                del(on[0])
                 time += 1
-        
-            
-        
-        
-    return time
+
+        return time+bridge_length
