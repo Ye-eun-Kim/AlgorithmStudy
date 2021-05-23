5395f43d8a298f9e4259c2c0d9a9b48dae321642
Ye-eun-Kim <8383655@naver.com>
2021-05-23 17:46:10 +0900

    분리한 후 삭제로 변경함

--- 210520.py
+++ 210520.py
@@ -1,9 +1,9 @@
 import re
 def solution(str1, str2):
-    str1 = re.sub('[^A-Za-z]', '', str1)
+    '''str1 = re.sub('[^A-Za-z]', '', str1)
     str1 = str1.upper()
     str2 = re.sub('[^A-Za-z]', '', str2)
-    str2 = str2.upper()
+    str2 = str2.upper()'''
     small = {}
     big = []
     _union = 0
@@ -19,12 +19,20 @@ def solution(str1, str2):
     short_len = len(short)
     for i in range(short_len-1):
         _key = short[i]+short[i+1]
+        _key = re.sub('[^A-Za-z]', '', _key)
+        _key = _key.upper()
+        if len(_key) != 2:
+            continue
         if _key in small:
             small[_key] +=1 
         else :
             small[_key] = 1
     for j in range(long_len-1):
         factor = long[j]+long[j+1]
+        factor = re.sub('[^A-Za-z]', '', factor)
+        factor = factor.upper()
+        if len(factor) != 2:
+            continue
         if factor in small :
             if small[factor] == 0:
                 _union = _union+1
@@ -34,12 +42,16 @@ def solution(str1, str2):
         else :
             _union+=1
     _union = _union + _intersection + sum(small.values())
-    _result = _intersection / _union * 65536
-    result = int(_result)
-    return result
+    if _union != 0:
+        _result = _intersection / _union * 65536
+        result = int(_result)
+        return result
+    else :
+        return 65536
+    
 
-str1 = "handshake"
-str2 = "shake hands"
+str1 = "E=M*C^2"
+str2 = "e=m*c^2"
 a = solution(str1, str2)
 print(a)
 
